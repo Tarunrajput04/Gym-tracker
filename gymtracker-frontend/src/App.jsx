b07a9b61024bb0a4730506ef7db7029b5041f6d4
Tarunrajput04 <tarunrajput1002@gmail.com>
2026-05-25 11:00:50 +0530

    Sprint 0 setup completed

---
+++ gymtracker-frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function App() {
+
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/hello')
+      .then(response => {
+        setMessage(response.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  )
+}
+
+export default App
